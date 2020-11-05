4025580e58b35a834cd9b52392a66c22c1f17f1d
Jesper Englund <jeengl@kth.se>
2020-11-05 13:59:29 +0100

    newstuff

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
 var myIndex = 0;
 //carousel();
 
-function carousel() {
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("mySlides");
+  var x = document.getElementsByClassName("portfolioSlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}
-  x[myIndex-1].style.display = "block";
-  setTimeout(carousel, 4000);
+  x[slideIndex-1].style.display = "block";
 }
 
 // Used to toggle the menu on small screens when clicking on the menu button
